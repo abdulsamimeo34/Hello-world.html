50c2dae0d1ddfdc18432f3fa90c2be35d07a236a
abdulsamimeo34 <160492688+abdulsamimeo34@users.noreply.github.com>
2024-02-26 12:44:18 +0500

    Add files via upload

---
+++ angry.py
@@ -0,0 +1,63 @@
+n=[h for h in range(1,101)]
+print(n)
+
+
+
+
+l=[]
+for g in range(1,101):
+    l.append(g)
+    print(l)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+k=("jkjkjkjkkjkjkjjjkjkkj")
+k.find('j')
+print(k)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+s=()
+for a in s:
+    print(a)
---
+++ bbb.py
@@ -0,0 +1,35 @@
+# w="welcomr     bbbbb  bbbe"
+# t=len(w)
+# print(t)
+# for a in range(t):
+#     print(w,[a])
+#
+#
+# n="the hunger man of world"
+# t=len(n)
+# for a in range(t):
+#     print(t,n)
+#
+m=100
+while m>=1:
+
+ print(m,'murder')
+ m=m-1
+
+
+ 
+
+
+#
+
+# while p<=10:
+#     print(p,"jack")
+#     p=p+1
+# #
+#  q="jack is the"
+#  print(q[3])
+#  print(q[1:4])
+#  print(q[1::5])
+w="hi my name is"
+n=w.capitalize() 
+print(n)
